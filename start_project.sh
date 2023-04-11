f48d073ffb5903a9e3e746691d6a672739980f21
lastuhinanton74@gmail.com <you@example.com>
2023-04-11 07:58:00 +0000

    update start_project.sh script

--- start_project.sh
+++ start_project.sh
@@ -3,3 +3,22 @@
 mkdir project
 touch ./project/Makefile
 mkdir ./project/srcs
+mkdir ./project/srcs/requirements
+mkdir ./project/srcs/requirements/bonus
+mkdir ./project/srcs/requirements/mariadb
+mkdir ./project/srcs/requirements/nginx
+mkdir ./project/srcs/requirements/tools
+mkdir ./project/srcs/requirements/wordpress
+mkdir ./project/srcs/requirements/wordpress/tools
+mkdir ./project/srcs/requirements/wordpress/conf
+touch ./project/srcs/requirements/wordpress/.dockerignore
+touch ./project/srcs/requirements/wordpress/Dockerfile
+mkdir ./project/srcs/requirements/nginx/tools
+mkdir ./project/srcs/requirements/nginx/conf
+touch ./project/srcs/requirements/nginx/.dockerignore
+touch ./project/srcs/requirements/nginx/Dockerfile
+mkdir ./project/srcs/requirements/mariadb/tools
+mkdir ./project/srcs/requirements/mariadb/conf
+touch ./project/srcs/requirements/mariadb/.dockerignore
+touch ./project/srcs/requirements/mariadb/Dockerfile
+
