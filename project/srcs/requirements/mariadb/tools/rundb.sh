b75bf05e3ca23bf81052004547f54ffacca497bd
lastuhinanton <lastuhinanton74@gmail.com>
2023-04-16 15:35:33 +0000

    add rundb.sh

---
+++ project/srcs/requirements/mariadb/tools/rundb.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# openrc
+# touch /run/openrc/softlevel
+
+# if [[ $(service mariadb status | awk '{print $NF}') != "started" ]]; then
+#     service mariadb setup
+#     service mariadb start
+# fi
+
+
+
+sh
