420e0b73296c64156c09e3c145756995c3d07d27
lastuhinanton <lastuhinanton74@gmail.com>
2023-04-17 14:22:11 +0000

    update check.sh

--- project/srcs/check.sh
+++ project/srcs/check.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
 
-tempVar=$(curl http://51.250.10.154/test/ 2>/dev/null | grep 'It is the test page!')
+tempVar=$(curl http://localhost/test/ 2>/dev/null | grep 'This page for test!')
 if [[ "$tempVar" ]] ; then
     printf "nginx is ${GREEN}RUNNING${NC}\n"
 else
