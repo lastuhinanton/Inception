04c553547b00cef6ad1769864121df110644ffb7
lastuhinanton <lastuhinanton74@gmail.com>
2023-04-24 19:02:27 +0000

    init testing in docker-compose

--- project/srcs/deleteData.sh
+++ project/srcs/deleteData.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo rm -rf ./data/
-sudo mkdir -p ./data/database ./data/www/ ./data/logs
+sudo rm -rf ./data/database/* ./data/www/* ./data/logs/* ./data/grafana_data/*
+sudo chmod -R 777 ./data/database/ ./data/www/ ./data/logs/ ./data/grafana_data/*
