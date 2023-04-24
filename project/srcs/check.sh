f007cfada50b4abf6b9c264b39cd09c6e64c1eaf
lastuhinanton <lastuhinanton74@gmail.com>
2023-04-24 16:25:01 +0000

    update check script

--- project/srcs/check.sh
+++ project/srcs/check.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-
-tempVar=$(curl http://localhost/test/ 2>/dev/null | grep 'This page for test!')
-if [[ "$tempVar" ]] ; then
-    printf "nginx is ${GREEN}RUNNING${NC}\n"
-else
-    printf "nginx is ${RED}NOT RUNNING${NC}\n"
-fi
+curl http://localhost:80 2>/dev/null
